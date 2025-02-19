a54e6d4249cec1c48560bde72e4bb9ccb2267414
StarKolya <mikola.starovoit@gmail.com>
2025-02-19 12:56:35 +0200

    Model developed

--- model.py
+++ model.py
@@ -4,16 +4,28 @@ from fastai.text.data import TextBlock
 from fastai.vision.all import *
 
 
+def label_func(name):
+    return "cat" if name.name[0].isupper() else "dog"
+
+
 # Download latest version
-path = kagglehub.dataset_download("tanlikesmath/the-oxfordiiit-pet-dataset")
+# path = kagglehub.dataset_download("tanlikesmath/the-oxfordiiit-pet-dataset")
+# print("Path to dataset files:", path)
 
-print("Path to dataset files:", path)
 
-
-dblock = DataBlock(
+dBlock = DataBlock(
     blocks=(ImageBlock, CategoryBlock),
     get_items=get_image_files,
     splitter=RandomSplitter(valid_pct=0.2, seed=42),
-    get_y=parent_label,
+    get_y=label_func,
     item_tfms=[Resize(192, method='squish')],
-)
+).dataloaders("images")
+
+
+learner = vision_learner(dBlock, resnet34, metrics=error_rate)
+learner.fine_tune(3)
+
+
+learner.export("model.pkl")
+
+
